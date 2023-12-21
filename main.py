a45cba120bcff9c1561799bbf48d8062fba7ad79
Darren <darrenlee6289@gmail.com>
2023-12-20 23:36:14 -0800

    v2 where I remove cost_organizer.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from cost_organizer import extract_data_from_file
-from receipt_api import receipt_processesor
+from receipt_api import receipt_processesor, image_to_json
+
 def main():
-    receipt_processesor("response2.json", "result.txt")
+    image = "image.png"
+    json_response = "response1.json"
+    result_response = "result.txt"
+
+    # image_to_json(image, json_response)
+    # receipt_processesor(json_response, result_response)
+    # items = extract_data_from_file(result_response)
+    
+    #image_to_json("image.png", "response1.json")
+    receipt_processesor("response1.json", "result.txt")
     items = extract_data_from_file("result.txt")
+
     for i in items:
         print(i, items[i])
 
--- receipt_api.py
+++ receipt_api.py
@@ -2,11 +2,27 @@ import json
 import pickle
 import requests
 
+url = "https://ocr.asprise.com/api/v1/receipt"
+
+def image_to_json(image_file, json_result_path):
+    res = requests.post(url,
+                        data = {
+                            'api_key': 'TEST',
+                            'recognizer': 'auto',
+                            'ref_no': 'oct_python_123'
+                        },
+                        files = {
+                            'file': open(image_file, 'rb')
+                        })
+
+    with open(json_result_path, "w") as f:
+        json.dump(json.loads(res.text), f)
+
 def receipt_processesor(file_path_json, file_path_result):
     with open(file_path_json, "r") as f:
         data = json.load(f)
 
-    receipts = data['receipts']
+    receipts = data['receipts'][0]
     items = receipts[0]['items']
     with open(file_path_result, "w") as f:
         for item in items:
